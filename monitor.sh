9e62050c1a0aab732154de268ee7d1886a574706
root <root@mds-vm01.operatorsubnet.ocidevcluster.oraclevcn.com>
2023-12-12 13:23:20 +0000

    add new functions

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3.9 mysqlstatus.py -h 10.0.10.78 -u admin -P'Welcome#1' --debug -m global
+# Debug option
+#python3.9 mysqlstatus.py -h 10.0.10.78 -u admin -P'Welcome#1' --debug -m global
+
+# Not Debug
+python3.9 mysqlstatus.py -h 10.0.10.78 -u admin -P'Welcome#1' -m global
