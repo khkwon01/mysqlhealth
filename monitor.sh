135634868e6b18439934fd579ce29ea97985c0c9
root <root@mds-vm01.operatorsubnet.ocidevcluster.oraclevcn.com>
2023-12-13 04:47:05 +0000

    modified code v3

--- monitor.sh
+++ monitor.sh
@@ -4,4 +4,4 @@
 #python3.9 mysqlstatus.py -h 10.0.10.78 -u admin -P'Welcome#1' --debug -m global
 
 # Not Debug
-python3.9 mysqlstatus.py -h 10.0.10.78 -u admin -P'Welcome#1' -m global
+python3.9 mysqlstatus.py -h 10.0.20.204 -u admin -P'Welcome#1' -m global
